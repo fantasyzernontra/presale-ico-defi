aa554ddc3410f457f8ffb01c74438c397574c105
fantasyzernontra <fantasyzernontra@hotmail.com>
2021-08-22 18:27:32 +0700

    :bulb: add Documentation Tag in IBEP20.sol

--- ico-contracts/contracts/libs/BEP20.sol
+++ ico-contracts/contracts/libs/BEP20.sol
@@ -1,3 +1,10 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.7.3;
+
+import "./IBEP20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+abstract contract BEP20 is IBEP20 {}
