3a9823abff06b61c2138c485265de2251496c045
fantasyzernontra <fantasyzernontra@hotmail.com>
2021-08-25 01:45:34 +0700

    :sparkles: add Complete all possible test case

--- ico-contracts/contracts/NonToken.sol
+++ ico-contracts/contracts/NonToken.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "./libs/BEP20.sol";
+import "./interfaces/BEP20.sol";
 
 contract NonToken is BEP20 {
     constructor() BEP20("NON Token", "NON") {}
--- ico-contracts/test/CrowedSale.js
+++ ico-contracts/test/CrowedSale.js
@@ -1,27 +1,62 @@
 const { expect } = require('chai')
 
+const { ToWei } = require('../utils/useToWei')
+
 // Crowed Sale Contract
 let CrowedSaleContract
 let crowedSale
 
+// NON Token Conract
+let NonToken
+let token
+
 // Constant Variables
-const FUND_ADDRESS = '0x56087c947475DF3200bCB53768236D0F1d788B36'
 const TOKEN_CAP = '150000'
 const PRICE = '0.25'
 
 beforeEach(async () => {
+	const [owner, addr1, addr2, adrr3] = await ethers.getSigners()
+
+	NonToken = await ethers.getContractFactory('NonToken')
+	token = await NonToken.deploy()
+
 	CrowedSaleContract = await ethers.getContractFactory('CrowedSale')
-	crowedSale = await CrowedSaleContract.deploy(
-		FUND_ADDRESS,
-		ethers.utils.parseUnits(TOKEN_CAP, 18),
-		ethers.utils.parseUnits(PRICE, 18)
-	)
+	crowedSale = await CrowedSaleContract.deploy(addr1.address, token.address, ToWei(TOKEN_CAP), ToWei(PRICE))
+
+	// Transfers ownership of NON Token to Crowed Sale.
+	await token.transferOwnership(crowedSale.address)
+	// Mints initial pool from specified token cap.
+	await token.mint(ToWei(TOKEN_CAP))
+	// Transfers NON Tokens to Crowed Sale's balance
+	await token.transfer(crowedSale.address, ToWei(TOKEN_CAP))
 })
 
 describe('Crowed Sale', () => {
 	it('Should deploy Crowed Sale contract successful', async () => {
+		expect(token.address)
 		expect(crowedSale.address)
 	})
 
-	// it('', async () => {})
+	it('Should transfer from Initial pool to Crowed Sale', async () => {
+		const [owner] = await ethers.getSigners()
+		expect(await token.balanceOf(owner.address)).to.equal(ToWei('0'))
+		expect(await token.balanceOf(crowedSale.address)).to.equal(ToWei(TOKEN_CAP))
+	})
+
+	it('Purchasing should successful and the beneficiary should recieve the exact amount of NON Token', async () => {
+		const [owner, addr1, addr2] = await ethers.getSigners()
+		const prov = await ethers.getDefaultProvider()
+		const buyingAmount = '1000'
+
+		await crowedSale.connect(addr2).purchaseToken({ value: ToWei(buyingAmount) })
+
+		const expectedRemainingInTokenCap = (parseInt(TOKEN_CAP) - parseInt(buyingAmount)).toString()
+
+		// Remaining NON token cap.
+		expect(await token.balanceOf(crowedSale.address)).to.equal(ToWei(expectedRemainingInTokenCap))
+		// New amount of NON Token.
+		expect(await token.balanceOf(addr2.address)).to.equal(ToWei(buyingAmount))
+
+		const balance = await prov.getBalance(addr1.address)
+	})
 })
